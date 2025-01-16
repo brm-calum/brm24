764968174e0f47732c0e653c4428d1247b83b5a1
brm-calum <calum@digitalpath.ee>
2025-01-16 09:31:07 +0200

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -148,7 +148,7 @@ export function Navbar({ className = '' }: { className?: string }) {
                 </div>
                 <Link
                   to="/contact"
-                  className="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-900 hover:text-green-700 rounded-md shadow-sm transition-colors"
+                  className="hidden sm:inline-flex items-center px-2 py-2 text-sm font-medium text-gray-900 hover:text-green-700 rounded-md shadow-sm transition-colors"
                 >
                   Contact BRM
                 </Link>
